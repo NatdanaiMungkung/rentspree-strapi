06df95b9e4bd348f1065112e9d09456c62d0442e
Natdanai Mungkung <natdanai@moneyguru.co.th>
2020-11-02 21:54:39 +0700

    WIP

---
+++ rent-spree/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '514951cfb269f61d24fb5175fa2db12d'),
+    },
+  },
+});
---
+++ rent-spree/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'be5766f4-6a9d-491a-a418-2617b059b8a3'
+};
